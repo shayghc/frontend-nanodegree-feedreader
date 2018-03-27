4cf4d00cc8ad942e760660e6a21674c9cb16bbfe
Seamus Connolly <sghconnolly@gmail.com>
2018-03-27 16:42:47 +0100

    feat: Write skeleton for the New Feed Selection test suite.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -111,9 +111,18 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
-
+    describe('New Feed Selection', function() {
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+         beforeEach(function() {
+            // function to check .feed container has completed loading.
+            loadFeed(0, function() {
+                done();
+            });
+         });
+
+         // Test here
+    });
 }());
