0324d9ed57dca74d35f1078305040f41de46f22a
Seamus Connolly <sghconnolly@gmail.com>
2018-03-27 13:42:14 +0100

    feat: Test that slide menu is hidden by default.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -52,17 +52,20 @@ $(function() {
                  });
              })(allFeeds[i]);
          };
-
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+    describe('The menu', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+         it('slide menu element is hidden by default', function() {
+            let target = $('body').attr('class'); // ? Move to beforeEach???*****************************
+            expect(target).toBe('menu-hidden');
+        });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
@@ -79,6 +82,7 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
